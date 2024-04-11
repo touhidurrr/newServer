d46cad28ce4b68726fda13c9e58731aa18d27dca
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2024-04-12 00:02:47 +0600

    improve info

--- src/routes/info.tsx
+++ src/routes/info.tsx
@@ -1,27 +1,73 @@
 import { Elysia } from 'elysia';
 import { html } from '@elysiajs/html';
 import { freemem, loadavg, totalmem } from 'os';
+import { format } from 'bytes';
+
+const usedmem = () => totalmem() - freemem();
+
+const ProgressBar = ({
+  id,
+  value,
+  max,
+  title,
+  info = '',
+}: {
+  id: string;
+  value: number;
+  max: number;
+  title: string;
+  info?: string;
+}) => (
+  <div style={{ minWidth: '20em', marginBottom: '20px', display: 'flex', flexDirection: 'column' }}>
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <label for={id} style={{ fontWeight: 'bold' }}>
+        {title}
+      </label>
+      <b>
+        <i>{info}</i>
+      </b>
+    </div>
+    <progress id={id} value={value} max={max} style={{ width: '100%', height: '20px' }}></progress>
+  </div>
+);
 
 export const infoPlugin = new Elysia().use(html()).get('/info', () => (
   <html lang="en">
     <head>
       <title>UncivServer.xyz Info Page</title>
     </head>
-    <body>
-      <div>
-        <label for="mem">Memory Usage:</label>
-        <br></br>
-        <progress id="mem" value={freemem()} max={totalmem()}></progress>
-      </div>
-      <div>
-        {[1, 5, 15].map((time, i) => (
-          <div>
-            <label for={`cpu-${i}`}>CPU (last {time} minutes)</label>
-            <br></br>
-            <progress id={`cpu-${i}`} value={loadavg()[i]} max={100}></progress>
-          </div>
-        ))}
-      </div>
+    <body
+      style={{
+        fontFamily: 'Arial, sans-serif',
+        margin: '0',
+        padding: '0',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        background: '#f0f0f0',
+      }}
+    >
+      <ProgressBar
+        id="memory"
+        value={usedmem()}
+        max={totalmem()}
+        title="Memory Usage"
+        info={`${format(usedmem())} / ${format(totalmem())}`}
+      />
+      {[1, 5, 15].map((time, i) => {
+        const usage = loadavg()[i];
+        return (
+          <ProgressBar
+            id={`cpu-${i}`}
+            value={usage}
+            max={100}
+            title={`CPU (last ${time} minutes)`}
+            info={`${usage.toFixed(2)}%`}
+          />
+        );
+      })}
     </body>
   </html>
 ));
