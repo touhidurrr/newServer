8151f14208ebb217a951ce5a31020386986c32e3
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2024-04-12 10:54:43 +0600

    use os.hostname() in app start test

--- tests/start.test.ts
+++ tests/start.test.ts
@@ -1,3 +1,4 @@
+import { hostname } from 'node:os';
 import { expect, test } from 'bun:test';
 import { DEFAULT_PORT, START_TEST_FETCH_TIMEOUT, START_TEST_TIMEOUT } from '@constants';
 
@@ -5,10 +6,11 @@ test(
   'App Start Test',
   async () => {
     const proc = Bun.spawn(['bun', 'start']);
+    const url = `http://${hostname()}:${DEFAULT_PORT}/isalive`;
 
     while (!proc.killed) {
       try {
-        const isAlive = await fetch(`http://localhost:${DEFAULT_PORT}/isalive`, {
+        const isAlive = await fetch(`http://}:${DEFAULT_PORT}/isalive`, {
           signal: AbortSignal.timeout(START_TEST_FETCH_TIMEOUT),
         }).then(res => res.text());
         if (isAlive) {
