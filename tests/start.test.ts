3a410e5e4e95a3896c025996be96e6d061e293a6
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2024-04-12 09:29:01 +0600

    fix app start test

--- tests/start.test.ts
+++ tests/start.test.ts
@@ -3,12 +3,11 @@ import { START_TEST_PORT, START_TEST_TIMEOUT } from '@constants';
 
 test(
   'App Start Test',
-  async () => {
+  async done => {
     const PORT = START_TEST_PORT;
     const proc = Bun.spawn(['bun', 'start'], { env: { PORT } });
 
-    while (true) {
-      if (proc.killed) return;
+    while (!proc.killed) {
       try {
         const isAlive = await fetch(`http://localhost:${PORT}/isalive`).then(res => res.text());
         if (isAlive) {
@@ -21,6 +20,7 @@ test(
 
     expect(proc.killed).toBe(false);
     proc.kill();
+    done();
   },
   START_TEST_TIMEOUT
 );
