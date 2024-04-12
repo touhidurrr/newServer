8e7fbe4bf7c93eb72b38903e8a340f3c93cc7255
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2024-04-12 10:32:22 +0600

    finally fix actions and use --bun flag during start

--- src/constants.ts
+++ src/constants.ts
@@ -17,4 +17,5 @@ export const GAME_ID_REGEX = /^[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}(_Preview)?
 // test
 export const START_TEST_PORT = '3030';
 export const START_TEST_TIMEOUT = 20_000;
+export const START_TEST_FETCH_TIMEOUT = 3_000;
 export const TEST_GAME_ID = '00000000-0000-0000-0000-000000000000';
--- tests/start.test.ts
+++ tests/start.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'bun:test';
-import { START_TEST_PORT, START_TEST_TIMEOUT } from '@constants';
+import { START_TEST_FETCH_TIMEOUT, START_TEST_PORT, START_TEST_TIMEOUT } from '@constants';
 
 test(
   'App Start Test',
@@ -9,7 +9,9 @@ test(
 
     while (!proc.killed) {
       try {
-        const isAlive = await fetch(`http://localhost:${PORT}/isalive`).then(res => res.text());
+        const isAlive = await fetch(`http://localhost:${PORT}/isalive`, {
+          signal: AbortSignal.timeout(START_TEST_FETCH_TIMEOUT),
+        }).then(res => res.text());
         if (isAlive) {
           expect(isAlive).toBeString();
           expect(isAlive).toBe('true');
