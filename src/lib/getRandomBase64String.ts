014c3e623dc8e35834a541bfd3d8c5d0f213ccf9
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2024-04-14 01:48:33 +0600

    some tiny cleanup

--- src/lib/getRandomBase64String.ts
+++ src/lib/getRandomBase64String.ts
@@ -1,9 +1,8 @@
 import bytes from 'bytes';
 
 export function getRandomBase64String(targetLength: number | string = 10): string {
-  if (typeof targetLength === 'string') {
-    targetLength = bytes.parse(targetLength);
-  }
+  if (typeof targetLength === 'string') targetLength = bytes.parse(targetLength);
+  if (targetLength > 65_536) throw new Error('Target length is too large');
   const arrSize = Math.ceil((targetLength * 3) / 4);
   const randArr = crypto.getRandomValues(new Uint8Array(arrSize));
   return Buffer.from(randArr).toString('base64');
